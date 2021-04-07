5bdde6563b572a8232f79ae4301a41e57b01540f
Fiona Cheong <fionacwt@icloud.com>
2021-04-08 00:58:03 +0800

    MyNotes and LocalView stylings

--- src/components/views/LocalView.vue
+++ src/components/views/LocalView.vue
@@ -1,7 +1,8 @@
 <template>
-    <div id="content">
-      <img id="bckgrnd" src="../../assets/Rectangle.png">
-      <router-link to="/mynotes" exact><img id="cross" src="../../assets/X.png"></router-link>
+    <div>
+      <div id= "localview">
+        <router-link to="/mynotes" exact><img id="cross" src="../../assets/X.png"></router-link>
+      </div>
     </div>
 </template>
 
@@ -12,13 +13,16 @@ export default {
 </script>
 
 <style scoped>
+#localview {
+  background: #47E4E4;
+  margin-top: 0%;
+  overflow: auto;
+}
 #cross {
     position: absolute;
     width: 50px;
     height: 50px;
     left: 40px;
     top: 140px;
-    display: flex;
-    align-items: center;
 }
 </style>
