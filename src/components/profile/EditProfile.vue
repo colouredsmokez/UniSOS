3964123b9c6beafb96e51d2ec24f7ef6e3185cca
claristoh <claristoh@Clariss-MacBook-Pro.local>
2021-04-09 22:53:11 +0800

    Added a function

--- src/components/profile/EditProfile.vue
+++ src/components/profile/EditProfile.vue
@@ -5,13 +5,33 @@
 
 <script>
 // import * as data from 'https://api.nusmods.com/v2/2020-2021/moduleList.json'
+import { db } from '../../firebase';
 
 export default {
-data() {
-    return {
-        // modules: data
+    data() {
+        return {
+            datapacket: {},
+            // modules: data
+        }
+    },
+    methods: {
+        fetchItems: function() {
+            db.collection('users').doc(this.uid).get().then((snapshot)=>{
+                this.datapacket = snapshot.data();   
+            })
+        },
+        updateProfile: function(attr) {
+            var newData = {}
+            for (var info in this.datapacket) {
+                newData[info] = info.value
+            }
+            var newVal = document.value;
+            if (newVal !== '') {
+                newData[attr] = newVal;
+            }
+            db.collection('users').doc(this.uid).update(newData).then(() => this.$router.push('myprofile'))
+        }
     }
-}
 }
 </script>
 
