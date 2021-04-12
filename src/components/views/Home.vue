1b80f3965a7e6d77aa4ab322d204d1fb97b2c3b0
Fiona Cheong <fionacwt@icloud.com>
2021-04-13 02:17:21 +0800

    rec css

--- src/components/views/Home.vue
+++ src/components/views/Home.vue
@@ -2,11 +2,12 @@
    <div>
       <div id="home">
         <img src= "../../assets/HomeBanner.png">
-        <h1>Recommends</h1>
+        <h1 id="rectxt">Recommends</h1>
       </div>
-      <div id="display">
-                <ul>
-                    <li id="listing" v-for="item in listingFiltered" v-bind:key="item" v-on:click="item.show = !item.show">
+      <div id="display"><hr>
+                <ul id="reclist">
+                    <li class="reclisting" v-for="item in listingFiltered" v-bind:key="item" v-on:click="item.show = !item.show">
+                        <div>
                         <div id="firstpart">
                             <h1>{{item.typeOfList}}</h1>
                             <div v-if="item.profilepic">
@@ -25,10 +26,6 @@
                         </div>
                         <div id="secondpart">
                             <h2>{{item.module}}</h2>
-                            <p>Took in: {{item.took_in}}</p>
-                            <p>Final grade: {{item.grade}}</p>
-                            <p v-if="item.typeOfList=='Notes'"> Price: ${{item.price}}<p>
-                            <p>{{item.addInfo}}</p>
                         </div>
                         <div id="thirdpart">
                             <div v-if="item.userId != currentUser">
@@ -36,9 +33,10 @@
                                 <button class="chat-button" v-bind:id="item.userId" v-on:click="toChat($event)"><i class="fa fa-comment" aria-hidden="true"></i> Chat</button>
                             </div>
                         </div>
+                        </div>
                     </li>
                 </ul>
-            </div>
+        </div>
     </div>
   
 </template>
@@ -108,10 +106,67 @@ export default {
 </script>
 
 <style scoped>
+@font-face {
+    font-family: 'FredokaOne';
+    src: url('/fonts/fredokaone-regular-webfont.woff2') format('woff2'),
+         url('/fonts/fredokaone-regular-webfont.woff') format('woff');
+    font-weight: normal;
+    font-style: normal;
+}
+#rectxt {
+    margin-left: 30px;
+}
 #home {
   background:  #47E4E4;
   margin-top: 0%;
   overflow: auto;
-  font-family: 'Trebuchet MS', 'Lucida Sans Unicode', 'Lucida Grande', 'Lucida Sans', Arial, sans-serif;
+  font-family: 'FredokaOne';
+}
+#display {
+    background:  #47E4E4;
+    overflow: auto;
+    font-family: 'FredokaOne';
+}
+#img {
+    object-fit: cover;
+    max-width: 100%;
+    max-height: 100vh;
+}
+#reclist {
+  width: 90%;
+  margin: 40px;
+  font-family: 'FredokaOne';
+  color: black;
+  display: flex;
+  flex-wrap: wrap;
+  list-style-type: none;
+  padding: 0;
+}
+ul#reclist li {
+    display: inline;
+}
+li {
+  text-align: center;
+  margin: 10px;
+}
+.reclisting {
+    text-align: center;
+    min-width: 20%;
+    max-width: 20%;
+    background: #47E4E4;
+    margin: 10px;
+}
+.image-cropper {
+  height: 90px;
+  width: 90px;
+  overflow: hidden;
+  border-radius: 50%;
+  align-content: center;
+}
+.profile-pic {
+  object-fit: cover;
+  height: 100%;
+  width: 100%;
+  align-content: center;
 }
 </style>
