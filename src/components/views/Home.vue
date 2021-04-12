8b89b419912247541920e1b44b21ab27bcdcd0a3
Fiona Cheong <fionacwt@icloud.com>
2021-04-13 03:19:19 +0800

    Recommendation list CSS

--- src/components/views/Home.vue
+++ src/components/views/Home.vue
@@ -1,24 +1,25 @@
 <template>
-   <div>
-      <div id="home">
+   <div id="home">
+      <div id="banner">
         <img src= "../../assets/HomeBanner.png">
-        <h1 id="rectxt">Recommends</h1>
+        
       </div>
 
       <div id="display">
+          <h1 id="rectxt">Recommended for you</h1>
                 <ul id="reclist">
                     <li id="reclisting" v-for="item in listingFiltered.sort(compare).slice(0,4)" v-bind:key="item" v-on:click="item.show = !item.show">
                         <div id="firstpart">
                             <h1>{{item.typeOfList}}</h1>
                             <div v-if="item.profilepic">
-                                <div class="image-cropper">
+                                <button class="chat-button" v-bind:id="item.userId" v-on:click="toChat($event)"><div class="image-cropper">
                                     <img :src="item.profilepic" alt="profilepic" class="profile-pic">
-                                </div>
+                                </div></button>
                             </div>
                             <div v-else>
-                                <div class="image-cropper">
+                                <button class="chat-button" v-bind:id="item.userId" v-on:click="toChat($event)"><div class="image-cropper">
                                     <img src="../../assets/defaultpfp.jpg" alt="profilepic" class="profile-pic">
-                                </div>
+                                </div></button>
                             </div>
                             <br>
                             <button class="profile-button" v-bind:id="item.userId" v-on:click="toProfile($event)">{{item.name}}</button>
@@ -29,10 +30,8 @@
                         </div>
                         <div id="thirdpart">
                             <div v-if="item.userId != currentUser">
-                                <button class="chat-button" v-bind:id="item.id" v-if="item.typeOfList=='Notes'" v-on:click="buy(item)">Buy</button>
-                                <button class="chat-button" v-bind:id="item.userId" v-on:click="toChat($event)"><i class="fa fa-comment" aria-hidden="true"></i> Chat</button>
+                                <button class="buy-button" v-bind:id="item.id" v-if="item.typeOfList=='Notes'" v-on:click="buy(item)">Buy</button>
                             </div>
-                        </div>
                         </div>
                     </li>
                 </ul>
@@ -131,7 +130,54 @@ export default {
           return 0
 
       }
-  }
+  },
+  
+        toProfile: function(event) {
+            let uid = event.target.getAttribute("id");
+            alert(uid);
+            this.$router.push({ name:'profile', params:{ uid:uid } });
+        },
+        toChat: function(event) {
+            let uid = event.target.getAttribute("id");
+            alert(uid);
+            this.$router.push({ name:'chat', params:{ uid:uid } });
+
+        },
+        buy: function(item) {
+            //alert(item.price);
+            
+
+            db.collection('users').doc(auth.currentUser.uid).get().then(
+                snapshot => {
+                    //console.log(snapshot)
+                    alert("Buy notes for $" + item.price + "?")
+                    var notesUpdaing = snapshot.data().myNotes
+                    //alert(notesUpdaing)
+                    if (notesUpdaing == null) {
+                        //alert("making new field")
+                        notesUpdaing = {}
+                    }
+                    var id = item.id
+                    
+                    if (id in notesUpdaing) {
+                        alert("Already Bought!")
+                    } else {
+                        
+                        notesUpdaing[id] = {} 
+                        notesUpdaing[id].imageURL = item.img;
+                        notesUpdaing[id].title = item.name + "'s " + item.module + " notes"
+                        notesUpdaing[id].ownerid = item.userId
+                        db.collection('users').doc(auth.currentUser.uid).update(
+                            {myNotes: notesUpdaing}
+                        )
+                    }
+                    
+                    
+                }
+            )
+
+            
+        }
     
   },
   created() {
@@ -154,7 +200,7 @@ export default {
 #rectxt {
     margin-left: 30px;
 }
-#home {
+#home, #banner {
   background:  #47E4E4;
   margin-top: 0%;
   overflow: auto;
@@ -164,6 +210,7 @@ export default {
     background:  #47E4E4;
     overflow: auto;
     font-family: 'FredokaOne';
+    text-align: center;
 }
 #img {
     object-fit: cover;
@@ -178,28 +225,37 @@ export default {
   display: flex;
   flex-wrap: wrap;
   list-style-type: none;
-  padding: 0;
+  padding:10px;
 }
 ul#reclist li {
     display: inline;
+    border-right:2px solid #C5ECEC;
+    width: 23.2%;
+    align-items: center;
+}
+ul#reclist li:last-child {
+    border-right:none;
 }
 li {
   text-align: center;
+  align-items: center;
   margin: 10px;
 }
 .reclisting {
     text-align: center;
-    min-width: 20%;
-    max-width: 20%;
+    min-width: 60%;
+    max-width: 60%;
     background: #47E4E4;
     margin: 10px;
 }
 .image-cropper {
-  height: 90px;
-  width: 90px;
+  height: 150px;
+  width: 150px;
   overflow: hidden;
   border-radius: 50%;
   align-content: center;
+  /*margin-left: 70px;
+  margin-right: 70px;*/
 }
 .profile-pic {
   object-fit: cover;
@@ -207,4 +263,44 @@ li {
   width: 100%;
   align-content: center;
 }
+.chat-button {
+    background-color: #C5ECEC;
+    height: 150px;
+    width: 150px;
+    border-radius: 50%;
+}
+.buy-button {
+    border-radius: 8px;
+    border: none;
+    padding: 10px 20px;
+    background-color:#C5ECEC;
+    color: black;
+    font-family: 'FredokaOne';
+    cursor: pointer;
+    transition-duration: 0.4s;
+    font-size: 20px;
+    margin:5px;
+}
+.buy-button:hover {
+    background-color: white;
+}
+#firstpart {
+    align-items: center;
+}
+.profile-button {
+    font-family: 'FredokaOne';
+    border-radius: 8px;
+    cursor: pointer;
+    font-size: 16px;
+    color: white;
+    border: none;
+    padding: 5px;
+    background-color: #25abb4;
+    cursor: pointer;
+    text-align: center;
+}
+.profile-button:hover {
+    background-color: black;
+}
+
 </style>
